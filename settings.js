f628f95a3c74d8fb1314fb41800230679d58efca
SAMITECH <117000938+FOSHONGSAMUEL@users.noreply.github.com>
2025-04-10 05:33:25 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "SAMITECH-AI",
+  botOwner: 'SAMITECH', // Your name
+  ownerNumber: '+237674137259', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
